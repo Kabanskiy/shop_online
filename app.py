f717ba8c1b0008c40a551678e27d81078c4b53b5
kabanskiy <yarikkabanskiy@mail.com>
2023-06-29 23:56:07 +0300

    создана БД

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
-db = SQLAlchemy
+app.config["SQLAlchemy_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 class Table(db.Model):
     id = db.Column(db.Integer, primary_key = True)
